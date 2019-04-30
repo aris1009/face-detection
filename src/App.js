e8f16cf48b55dc014b2a3c8ccc285329a021b8a1
Aris Konstantoulas <arist.kon@gmail.com>
2019-05-01 01:15:38 +0300

    Added particles.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import React from 'react';
-import NavigationBar from './Components/NavigationBar/NavigationBar.js';
-import Logo from './Components/Logo/Logo.js';
+import Particles from 'react-particles-js';
+import NavigationBar from './Components/NavigationBar/NavigationBar';
+import Logo from './Components/Logo/Logo';
+import ImageForm from './Components/ImageForm/ImageForm';
+import './Components/Logo/Logo.css';
 import './App.css';
 
+const particleParams = {
+  "particles": {
+    "number": {
+      "value": 40,
+      density: {
+        enable: true,
+        value_area: 400
+      }
+    },
+    "size": {
+      "value": 2
+    }
+  }
+}
+
 function App() {
-	return (<div className="App">
-		<NavigationBar />
-		<Logo/>
-			{/* <ImageForm/>
-		<FaceDetectSquare/> */}
-	</div>);
+  return (
+    <div className="App">
+      <Particles className="particles" params={particleParams} />
+      <NavigationBar />
+      <Logo />
+      {/* TODO <Rank /> */}
+      <ImageForm />
+      {/* <FaceDetectSquare/> */}
+    </div>);
 }
 
 export default App;
--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,30 +1,16 @@
 import React, { Component } from 'react';
-import './Logo.css';
 
 export default class Logo extends Component {
     render() {
         return (
             <div>
-                <style>
-                    .logo {
-                    display: block;}
-                    // position: relative;
-                    // margin: 50px auto 0;
-                    // padding: 10px 0;
-                    // width: 110px;
-                    // color: #fff;
-                    // text-align: center;
-                    // text-transform: uppercase;
-                    // background-color: #0099cc;
-                    // border: none;
-                //   }
-                </style>
-                <span className="logo">
-                    Aris
+                <span className="logo pa3 ma3
+                 ba bw1 b--purple bg-lightest-blue
+                 hover-bg-purple hover-white
+                 pointer grow">
+                    Face Recognition
                 </span>
             </div>
         )
     }
 }
-
-// pa3 ma2 ba b--purple
